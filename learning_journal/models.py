c42fdd4e788c39c13f5b178ef13842354a346efd
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2015-01-12 22:45:49 -0800

    added entry model and class methods

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -1,8 +1,13 @@
+import datetime
+
+
 from sqlalchemy import (
     Column,
     Index,
     Integer,
     Text,
+    DateTime,
+    Unicode
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -25,3 +30,19 @@ class MyModel(Base):
     value = Column(Integer)
 
 Index('my_index', MyModel.name, unique=True, mysql_length=255)
+
+class Entry(Base):
+    __tablename__ = 'entries'
+    id = Column(Integer, primary_key=True)
+    title = Column(Text, unique=True, nullable=False) #how to add 255 max?
+    body = Column(Text, default=u'')
+    created = Column(DateTime, default=datetime.datetime.utcnow)
+    edited = Column(DateTime, default=datetime.datetime.utcnow)
+
+    @classmethod
+    def by_id(cls, id):
+        return DBSession.query(Entry).filter(Entry.id == id).first()
+
+    @classmethod
+    def all(cls):
+        return DBSession.query(Entry).order_by(sa.desc(Entry.created))
